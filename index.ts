e78b41541802dbe517752f60a5fd9ec9e5fb106f
smolgrrr <smolgrrr@protonmail.com>
2023-11-08 23:35:30 +1100

    looks like it works

--- index.ts
+++ index.ts
@@ -1,98 +1,162 @@
 import * as Nostr from "nostr-tools";
 import { RelayPool } from "nostr-relaypool";
 import dotenv from "dotenv";
+import { Event, finishEvent } from "nostr-tools";
+import { EventEmitter } from 'events';
+
+const eventEmitter = new EventEmitter();
 
 dotenv.config();
 
 const MyPubkey = process.env.BOT_PUBKEY;
 const MySecret = process.env.BOT_SECRET;
 
+/** Get POW difficulty from a Nostr hex ID. */
+function getPow(hex: string): number {
+  let count = 0
+
+  for (let i = 0; i < hex.length; i++) {
+    const nibble = parseInt(hex[i], 16)
+    if (nibble === 0) {
+      count += 4
+    } else {
+      count += Math.clz32(nibble) - 28
+      break
+    }
+  }
+
+  return count
+}
+
+type RepostEventTemplate = {
+  /**
+   * Pass only non-nip18 tags if you have to.
+   * Nip18 tags ('e' and 'p' tags pointing to the reposted event) will be added automatically.
+   */
+  tags?: string[][]
+
+  /**
+   * Pass an empty string to NOT include the stringified JSON of the reposted event.
+   * Any other content will be ignored and replaced with the stringified JSON of the reposted event.
+   * @default Stringified JSON of the reposted event
+   */
+  content?: ''
+
+  created_at: number
+}
+
+function finishRepostEvent(
+  t: RepostEventTemplate,
+  reposted: Event<number>,
+  relayUrl: string,
+  privateKey: string,
+): Event {
+  return finishEvent(
+    {
+      kind: 6,
+      tags: [...(t.tags ?? []), ['e', reposted.id, relayUrl], ['p', reposted.pubkey]],
+      content: t.content === '' ? '' : JSON.stringify(reposted),
+      created_at: t.created_at,
+    },
+    privateKey,
+  )
+}
+
 const pool = new RelayPool(undefined, {
   autoReconnect: true,
   logErrorsAndNotices: true,
 });
 
+const checkRepostsPool = new RelayPool(undefined, {
+  autoReconnect: true,
+  logErrorsAndNotices: true,
+});
+
 const feedRelays = [
-  "wss://relay-jp.nostr.wirednet.jp/",
-  "wss://nostr-relay.nokotaro.com/",
-  "wss://nostr.h3z.jp/",
-  "wss://nostr.holybea.com/",
-  "wss://yabu.me/",
+  "wss://relay.snort.social",
+  "wss://nostr.bitcoiner.social",
+  "wss://nostr.mom",
+  "wss://relay.nostr.bg",
+  "wss://nos.lol",
+  "wss://powrelay.xyz"
 ];
 
-pool.subscribe(
+let poolEventIds: string[] = [];
+
+checkRepostsPool.subscribe(
   [
     {
-      kinds: [1],
-      since: Math.floor(new Date().getTime() / 1000 - 30 * 60),
+      kinds: [6],
+      authors: [MyPubkey as string],
     },
   ],
   feedRelays,
   (event, _isAfterEose, _relayURL) => {
-    if (filterEvent(event)) {
-      replyEvent(event);
+    // Add event ID to the list
+    poolEventIds.push(event.id);
+
+    // Emit an event when the array is populated
+    if (poolEventIds.length > 0) {
+      eventEmitter.emit('poolEventIdsPopulated');
     }
   }
 );
 
+eventEmitter.on('poolEventIdsPopulated', () => {
+  pool.subscribe(
+    [
+      {
+        kinds: [1],
+        since: Math.floor((Date.now() * 0.001) - (15 * 60)),
+      },
+    ],
+    feedRelays,
+    (event, _isAfterEose, _relayURL) => {
+      if (filterEvent(event)) {
+        repostEvent(event);
+      }
+    }
+  );
+});
+
 function filterEvent(event: Nostr.Event): boolean {
-  if (event.pubkey === MyPubkey) {
+  // Check if event ID exists in the list
+  const eTag = event.tags.filter(tag => tag[0] === 'e').map(tag => tag[1]);
+  if (poolEventIds.includes(eTag[0])) {
+    console.log(event.id + 'REJECTED!?!?!')
     return false;
   }
 
-  if (event.content.match(/こじら[さん|氏|にゃん]/)) {
-    return false;
-  } else if (event.content.match(/こじら[さ-それず]/)) {
+  if (getPow(event.id) > 20 && !event.tags.some((tag) => tag[0] === "e")) {
     return true;
   }
   return false;
 }
 
-function replyEvent(event: Nostr.Event): void {
+let lastRepostTime = 0;
+
+function repostEvent(event: Nostr.Event): void {
+  const currentTime = Date.now();
+  const fifteenMinutesInMilliseconds = 15 * 60 * 1000;
+
+  // Check if 15 minutes have passed since the last repost
+  if (currentTime - lastRepostTime < fifteenMinutesInMilliseconds) {
+    console.log('Less than 15 minutes have passed since the last repost. Skipping this event.');
+    return;
+  }
   console.log(JSON.stringify({ event }));
-  const parsedReplyTags = Nostr.nip10.parse(event);
-  console.log(JSON.stringify({ parsedReplyTags }));
 
-  const tags: string[][] = [];
-  if (parsedReplyTags.root) {
-    tags.push(["e", parsedReplyTags.root.id, "", "root"]);
-    for (let i = 0; i < parsedReplyTags.mentions.length; ++i) {
-      const m = parsedReplyTags.mentions[i];
-      tags.push(["e", m.id, ""]);
-    }
-    tags.push(["e", event.id, "", "reply"]);
-  } else {
-    tags.push(["e", event.id, "", "root"]);
-    for (let i = 0; i < parsedReplyTags.mentions.length; ++i) {
-      const m = parsedReplyTags.mentions[i];
-      tags.push(["e", m.id, ""]);
-    }
+  const template = {
+    created_at: Math.floor(Date.now() / 1000),
   }
+  const repostEvent = finishRepostEvent(template, event, feedRelays[feedRelays.length - 1], MySecret as string)
 
-  for (let i = 0; i < parsedReplyTags.profiles.length; ++i) {
-    const p = parsedReplyTags.profiles[i];
-    tags.push(["p", p.pubkey]);
-  }
-  tags.push(["p", event.pubkey]);
+  const validateEvent = Nostr.validateEvent(repostEvent);
+  const verifySignature = Nostr.verifySignature(repostEvent);
+  console.log(JSON.stringify({ validateEvent, verifySignature, repostEvent }));
 
-  console.log(JSON.stringify({ tags }));
+  pool.publish(repostEvent as Event, feedRelays);
 
-  const replyEvent = {
-    sig: "",
-    id: "",
-    kind: 1,
-    tags,
-    pubkey: MyPubkey ?? "",
-    content: "呼んだ？",
-    created_at: event.created_at + 1,
-  };
-
-  replyEvent.id = Nostr.getEventHash(replyEvent);
-  replyEvent.sig = Nostr.getSignature(replyEvent, MySecret ?? "");
-
-  const validateEvent = Nostr.validateEvent(replyEvent);
-  const verifySignature = Nostr.verifySignature(replyEvent);
-  console.log(JSON.stringify({ validateEvent, verifySignature, replyEvent }));
-
-  pool.publish(replyEvent, feedRelays);
+    // Update the last repost time
+    lastRepostTime = currentTime;
 }
