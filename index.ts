07e9859a4243f053e139b8431f00cc95a951a0e8
smolgrrr <smolgrrr@protonmail.com>
2023-11-09 14:38:29 +1100

    difficulty adjustment

--- index.ts
+++ index.ts
@@ -67,11 +67,6 @@ const pool = new RelayPool(undefined, {
   logErrorsAndNotices: true,
 });
 
-const checkRepostsPool = new RelayPool(undefined, {
-  autoReconnect: true,
-  logErrorsAndNotices: true,
-});
-
 const feedRelays = [
   "wss://relay.snort.social",
   "wss://nostr.bitcoiner.social",
@@ -81,67 +76,62 @@ const feedRelays = [
   "wss://powrelay.xyz"
 ];
 
-let poolEventIds: string[] = [];
-
-checkRepostsPool.subscribe(
+pool.subscribe(
   [
     {
-      kinds: [6],
-      authors: [MyPubkey as string],
+      kinds: [1],
+      since: Math.floor((Date.now() * 0.001) - (60 * 60)),
     },
   ],
   feedRelays,
   (event, _isAfterEose, _relayURL) => {
-    // Add event ID to the list
-    poolEventIds.push(event.id);
-
-    // Emit an event when the array is populated
-    if (poolEventIds.length > 0) {
-      eventEmitter.emit('poolEventIdsPopulated');
+    if (filterEvent(event)) {
+      repostEvent(event);
     }
   }
 );
 
-eventEmitter.on('poolEventIdsPopulated', () => {
-  pool.subscribe(
-    [
-      {
-        kinds: [1],
-        since: Math.floor((Date.now() * 0.001) - (15 * 60)),
-      },
-    ],
-    feedRelays,
-    (event, _isAfterEose, _relayURL) => {
-      if (filterEvent(event)) {
-        repostEvent(event);
-      }
-    }
-  );
-});
+let lastRepostTime = 0;
+let difficulty = 20;
+let eventCount = 0;
+let lastAdjustmentTime = Date.now();
 
 function filterEvent(event: Nostr.Event): boolean {
-  // Check if event ID exists in the list
-  const eTag = event.tags.filter(tag => tag[0] === 'e').map(tag => tag[1]);
-  if (poolEventIds.includes(eTag[0])) {
-    console.log(event.id + 'REJECTED!?!?!')
-    return false;
+  const currentTime = Date.now();
+  const twelveHoursInMilliseconds = 12 * 60 * 60 * 1000;
+
+  // Check if 12 hours have passed since the last difficulty adjustment
+  if (currentTime - lastAdjustmentTime >= twelveHoursInMilliseconds) {
+    // Adjust difficulty based on the number of events in the last 12 hours
+    // We want approximately 24 events per 12 hours
+    if (eventCount > 24) {
+      difficulty++;
+    } else if (eventCount < 24) {
+      difficulty = Math.max(0, difficulty - 1); // Ensure difficulty doesn't go below 0
+    }
+
+    // Reset event count and last adjustment time
+    eventCount = 0;
+    lastAdjustmentTime = currentTime;
+    console.log('Current difficulty: ', difficulty);
   }
 
-  if (getPow(event.id) > 20 && !event.tags.some((tag) => tag[0] === "e")) {
+  if (getPow(event.id) > difficulty && !event.tags.some((tag) => tag[0] === "e")) {
+    console.log('found PoW event: ', event.id)
+    eventCount++;
     return true;
   }
+
   return false;
 }
 
-let lastRepostTime = 0;
-
 function repostEvent(event: Nostr.Event): void {
   const currentTime = Date.now();
-  const fifteenMinutesInMilliseconds = 15 * 60 * 1000;
+  const thirtyMinutesInMilliseconds = 30 * 60 * 1000;
 
-  // Check if 15 minutes have passed since the last repost
-  if (currentTime - lastRepostTime < fifteenMinutesInMilliseconds) {
-    console.log('Less than 15 minutes have passed since the last repost. Skipping this event.');
+  // Check if 30 minutes have passed since the last repost
+  if (currentTime - lastRepostTime < thirtyMinutesInMilliseconds) {
+    console.log('Less than 30 minutes have passed since the last repost. Skipping this event.');
     return;
   }
   console.log(JSON.stringify({ event }));
@@ -157,6 +147,6 @@ function repostEvent(event: Nostr.Event): void {
 
   pool.publish(repostEvent as Event, feedRelays);
 
-    // Update the last repost time
-    lastRepostTime = currentTime;
+  // Update the last repost time
+  lastRepostTime = currentTime;
 }
